79d47a8f5d7a4c402a2f84eb4e42c4c48f1c83a6
jnorberto23 <joaovictor.norberto@outlook.com>
2023-02-17 12:12:03 -0300

    ✨add createTask styles; add phosphor-icon dependency

--- src/components/CreateTask.tsx
+++ src/components/CreateTask.tsx
@@ -1,14 +1,17 @@
+import { PlusCircle } from "phosphor-react";
 import styles from "./CreateTask.module.css";
 
 export function CreateTask() {
   return (
     <div className={styles.container}>
-      <div className={styles.wrapper}>
-        <form action="">
+      <form action="">
+        <div className={styles.wrapper}>
           <input type="text" placeholder="Adicione uma nova tarefa" />
-          <button type="submit">Criar</button>
-        </form>
-      </div>
+          <button type="submit" title="Criar">
+            Criar <PlusCircle size={20} />
+          </button>
+        </div>
+      </form>
     </div>
   );
 }
