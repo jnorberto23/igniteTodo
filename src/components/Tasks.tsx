f3132fa6bc6f636388e1c7b3d04f4ee0fbd4c7fe
jnorberto23 <joaovictor.norberto@outlook.com>
2023-02-17 16:00:27 -0300

    🚧work on task component

--- src/components/Tasks.tsx
+++ src/components/Tasks.tsx
@@ -3,9 +3,17 @@ export function Tasks() {
   return (
     <article className={styles.container}>
       <div className={styles.tasksInfo}>
-        <strong className={styles.tasksInfoCreated}>Tarefas criadas</strong>
-        <strong className={styles.tasksInfoDone}>Concluídas</strong>
+        <strong className={styles.tasksInfoCreated}>
+          Tarefas criadas
+          <span>2</span>
+        </strong>
+        <strong className={styles.tasksInfoDone}>
+          Concluídas
+          <span>2</span>
+        </strong>
       </div>
+
+      <div></div>
     </article>
   );
 }
