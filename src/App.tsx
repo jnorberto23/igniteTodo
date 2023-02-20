d48b7c23a1e19bfa95554a7c0254c644c425410b
jnorberto23 <joaovictor.norberto@outlook.com>
2023-02-20 14:41:21 -0300

    ♻️refactor Header, App and Tasks components styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
+import { Fragment } from "react";
 import { CreateTask } from "./components/CreateTask";
 import { Header } from "./components/Header";
 import { Tasks } from "./components/Tasks";
+import styles from "./App.module.css";
 import "./global.css";
 
 export function App() {
   return (
-    <div>
-      <Header />
-      <CreateTask />
-      <Tasks />
-    </div>
+    <Fragment>
+      <main className={styles.container}>
+        <Header />
+        <CreateTask />
+        <Tasks />
+      </main>
+    </Fragment>
   );
 }
--- src/components/Tasks.tsx
+++ src/components/Tasks.tsx
@@ -1,8 +1,8 @@
 import styles from "./Tasks.module.css";
 export function Tasks() {
   return (
-    <article className={styles.container}>
-      <div className={styles.tasksInfo}>
+    <article>
+      <article className={styles.tasksInfo}>
         <strong className={styles.tasksInfoCreated}>
           Tarefas criadas
           <span>2</span>
@@ -11,16 +11,15 @@ export function Tasks() {
           Concluídas
           <span>2</span>
         </strong>
-      </div>
-
-      {/* <fieldset className={styles.contentList}>
+      </article>
+      <fieldset className={styles.contentList}>
         <label className={styles.contentItem}>
           <input type="checkbox" className={styles.checkboxRound} /> checkbox 1
         </label>
         <label className={styles.contentItem}>
           <input type="checkbox" className={styles.checkboxRound} /> checkbox 2
         </label>
-      </fieldset> */}
+      </fieldset>
     </article>
   );
 }
